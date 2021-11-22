02a92d5593320ae8904f43d5d5fe817d1b5b16cf
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-11-22 17:15:55 +0500

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+import {Header}  from './Header/Header'
+import {HeroSec} from './HeroSec/HeroSec'
+function App() {
+  return (
+  <div>
+<Header/>
+<HeroSec/>
+</div>
+);}
+
+export default App;
