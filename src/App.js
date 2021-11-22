7bf39efa4a6272e03c356fdd2f7959423a24d69b
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-11-22 18:50:59 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@
 import './App.css';
 import {Header}  from './Header/Header'
 import {HeroSec} from './HeroSec/HeroSec'
+import Studysec from './StudySec/Studysec'
 function App() {
   return (
   <div>
 <Header/>
 <HeroSec/>
+<Studysec/>
 </div>
 );}
 
