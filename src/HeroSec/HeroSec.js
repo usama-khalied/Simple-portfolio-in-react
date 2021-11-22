31bc20b0291ff3751c3a570572674b0bb3c1f080
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-11-22 21:12:07 +0500

    Update HeroSec.js

--- src/HeroSec/HeroSec.js
+++ src/HeroSec/HeroSec.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import HeroSecCss from './HeroSec.module.css'
-import  {ScrollContainer }
+import { ScrollContainer , ScrollPage , Sticky , Animator} from 'react-scroll'
 export const HeroSec = () => {
     return (
     <>
+    <ScrollContainer>
+        <ScrollPage page={0}>
+            <Animator animation={Sticky()}> 
 <section  className={`${HeroSecCss['hero-sec']}`}>
 <div className={`${HeroSecCss['hero-text']}`}>
 <h1> We’re Creative <br/>
 We Ensure Quality Design</h1>
 
-
 </div>
-
-
-
-
-
-
 </section>
-          
+</Animator>
+</ScrollPage>
+</ScrollContainer>          
 
         </>
     )
