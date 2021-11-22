dc0b3547fae04104ebfc1d1a1186a178cfb32f3d
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-11-22 17:12:07 +0500

    Create HeroSec.js

---
+++ src/HeroSec/HeroSec.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import HeroSecCss from './HeroSec.module.css'
+export const HeroSec = () => {
+    return (
+    <>
+<section  className={`${HeroSecCss['hero-sec']}`}>
+<div className={`${HeroSecCss['hero-text']}`}>
+<h1> We’re Creative <br/>
+We Ensure Quality Design</h1>
+
+
+</div>
+
+
+
+
+
+
+</section>
+          
+
+        </>
+    )
+}
+
