e44dd3ccfbccc871e329e2ac4c36684333b410e9
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-04 19:22:40 +0500

    Update Client.js

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -8,7 +8,7 @@ export default class SimpleSlider extends Component{
         const settings = {
           dots: true,
           
-          infinite: false,
+          infinite:true,
           speed: 500,
           autoplay : true,
           slidesToShow: 1,
@@ -25,17 +25,13 @@ export default class SimpleSlider extends Component{
       
         <Slider className={`${ClientCss.slider}`} {...settings}>
           <div>
-            <h3>1</h3>
-            <h3>1</h3>
-            <h3>1</h3>
-            <h3>1</h3>
+            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. </p>
+          
 
           </div>
           <div>
-            <h3>2</h3>
-            <h3>2</h3>
-            <h3>2</h3>
-            <h3>2</h3>
+          <p> Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop of Lorem Ipsum.</p>
+          
 
           </div>
          
