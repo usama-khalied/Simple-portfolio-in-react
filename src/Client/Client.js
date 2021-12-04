e4fc34dd121b4aa7cb5a8635af644e5eb5ddd69d
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-04 18:48:44 +0500

    Update Client.js

--- src/Client/Client.js
+++ src/Client/Client.js
@@ -1,10 +1,69 @@
-import React from 'react'
+import React , { Component } from 'react'
 import ClientCss from './Client.module.css'
-export default function Client() {
+
+import Slider from "react-slick";
+
+export default class SimpleSlider extends Component{
+    render () {
+        const settings = {
+          dots: true,
+          infinite: false,
+          speed: 1000,
+          slidesToShow: 1,
+          slidesToScroll: 1
+        };
     return (
-<>
+<div>
+<section className={`${ClientCss.Testi}`}>
+<div className={`${ClientCss.studytext}`}>
+                    <h1> Clients Opinion</h1>
+                    <small> </small>
+                </div>
+                <div>
+      
+        <Slider className={`${ClientCss.slider}`} {...settings}>
+          <div>
+          <h3>2</h3>
+            <p> lorem
+            </p>
+            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>
+          </div>
+          <div>
+            <h3>2</h3>
+            <p> lorem
+            </p>
+            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>            <h3>2</h3>
+            <p> lorem
+            </p>
+          </div>
+        </Slider>
+      </div>
 
+</section>
 
-</>
-    )
+</div>
+    );
+}
 }
