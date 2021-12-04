36ef806ca8654a8429ae744efbb50f8f9a76d32d
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-04 18:48:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 import {Header}  from './Header/Header'
 import {HeroSec} from './HeroSec/HeroSec'
 import Studysec from './StudySec/Studysec'
 import BankSec from './BankSec/BankSec'
+import Client from './Client/Client';
 function App() {
   return (
   <div>
@@ -11,6 +14,7 @@ function App() {
 <HeroSec/>
 <Studysec/>
 <BankSec/>
+<Client/>
 </div>
 );}
 
