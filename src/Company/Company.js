b22826ba759b7cfe512c4f10a30c27fc3a6ee9d5
ZaidebinUsama <77499086+usama-khalied@users.noreply.github.com>
2021-12-13 21:58:34 +0500

    Create Company.js

---
+++ src/Company/Company.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import CompanyCss from './Company.module.css'
+export default function Company() {
+    return (
+<>
+<section className={`${CompanyCss.Company}`}>
+
+
+
+</section>
+
+
+
+
+</>
+    )
+}
